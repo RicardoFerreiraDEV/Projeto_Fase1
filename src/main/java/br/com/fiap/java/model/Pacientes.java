07b67dab6385a2bd49689da2601a2ae881ef6892
Ricardo <ricardo_santos_25@carrefour.com>
2021-07-27 23:41:19 -0300

    Model Pacientes

--- src/main/java/br/com/fiap/java/model/Pacientes.java
+++ src/main/java/br/com/fiap/java/model/Pacientes.java
@@ -1,5 +1,9 @@
 package br.com.fiap.java.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,10 +12,26 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Pacientes {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Long idPacientes;
+    private Long idVacina;
+    private Long idPontoVacinacao;
+    private String nome;
+    private LocalDate dataNac;
+    private String endereco;
+    private String dose;
+    private LocalDate dataVasc;
+    private String telefone;
+    private String nomeMae;
+    private String nomePai;
+    private String sexo;
+    private String cpf;
+    private String grupoPrioridade;
+    private String racaCor;
 }
