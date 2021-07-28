28f772c982d14ee74c1e05a91409d8b1e6af3d59
Ricardo <ricardo_santos_25@carrefour.com>
2021-07-27 23:46:48 -0300

    Model Vacinas

--- src/main/java/br/com/fiap/java/model/Vacinas.java
+++ src/main/java/br/com/fiap/java/model/Vacinas.java
@@ -1,15 +1,29 @@
 package br.com.fiap.java.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cglib.core.internal.LoadingCache;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Vacinas {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idVacina;
+    private Long idPontoVacinacao;
+    private Long idPaciente;
+    private Long nomeVacina;
+    private LocalDate vencimento;
+    private String lote;
 
 }
