e466df42aa10910d4b707a82dc3ee5581d105363
Ricardo <ricardo_santos_25@carrefour.com>
2021-07-27 23:51:25 -0300

    Model Ponto Vacinacao

--- src/main/java/br/com/fiap/java/model/PontosVacinacao.java
+++ src/main/java/br/com/fiap/java/model/PontosVacinacao.java
@@ -1,15 +1,25 @@
 package br.com.fiap.java.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class PontosVacinacao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    private Long idPontoVacinacao;
+    private Long idVacina;
+    private Long idPaciente;
+    private Long enderecoVacinacao;
+    private Long tipoLocal;
 }
